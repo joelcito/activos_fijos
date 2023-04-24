40fc1a7ef0fe1b33225364af5b196225de985184
Joel Flores <jjjoelcito123@gmail.com>
2023-04-24 04:57:43 -0400

    se agrego los dique

--- src/main/java/com/activo/fijos/controller/ExternoRestController.java
+++ src/main/java/com/activo/fijos/controller/ExternoRestController.java
@@ -3,9 +3,13 @@ package com.activo.fijos.controller;
 import java.util.List;
 import java.util.Map;
 
+import org.json.JSONObject;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,11 +24,14 @@ public class ExternoRestController {
 		this.jdbcTemplate = jdbcTemplate;
 	}
 	
-	@GetMapping("/login")
-	public List<Map<String, Object>> prueba() {
-		System.out.println("como es");
-		List<Map<String, Object>> usuarios = jdbcTemplate.queryForList("SELECT * FROM rh_usuario");
-		//return ResponseEntity.ok(usuarios);
-		return usuarios;
+	@PostMapping("/login")
+	//public List<Map<String, Object>> prueba() {
+	public void prueba(@RequestBody Map<String, String> user) {
+		System.out.println("Email: " + user.get("email"));
+        System.out.println("Password: " + user.get("password"));
+		//System.out.println("como es", email);
+		//List<Map<String, Object>> usuarios = jdbcTemplate.queryForList("SELECT * FROM rh_usuario");
+		
+		//return usuarios;
 	}
 }
