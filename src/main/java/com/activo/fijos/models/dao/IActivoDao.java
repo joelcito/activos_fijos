2e1579f4fa05ee9667a6841fa9a4418528b9236b
Joel Flores <jjjoelcito123@gmail.com>
2023-05-05 08:43:41 -0400

    se agrego losc ambios

--- src/main/java/com/activo/fijos/models/dao/IActivoDao.java
+++ src/main/java/com/activo/fijos/models/dao/IActivoDao.java
@@ -24,10 +24,12 @@ public interface IActivoDao extends CrudRepository<Activo, String> {
 	//@Query(value="SELECT idactivo, descripcion, precio FROM afw_activo", nativeQuery=true)
 	public List<Map<String, Object>> listaActivosPer();
 		
-	@Query(value="SELECT idactivo, codigo, estado, fechacompra, regional_id, descripcion FROM afw_activo WHERE codigo = :codigo", nativeQuery=true)
+	//@Query(value="SELECT idactivo, codigo, estado, fechacompra, regional_id, descripcion FROM afw_activo WHERE codigo = :codigo", nativeQuery=true)
+	@Query(value="SELECT af.idactivo, af.codigo, af.estado, af.fechacompra, af.descripcion, re.nombre FROM afw_activo af INNER JOIN afw_regional re ON af.regional_id = re.idregional WHERE codigo = :codigo  ORDER BY af.fechacreacion DESC", nativeQuery=true)
 	public List<Map<String, Object>> buscaActivo(String codigo);
 	
-	@Query(value="SELECT idactivo, codigo, estado, fechacompra, regional_id, descripcion FROM afw_activo WHERE descripcion LIKE %:descripcion%", nativeQuery=true)
+	//@Query(value="SELECT idactivo, codigo, estado, fechacompra, regional_id, descripcion FROM afw_activo WHERE descripcion LIKE %:descripcion%", nativeQuery=true)
+	@Query(value="SELECT af.idactivo, af.codigo, af.estado, af.fechacompra, af.descripcion, re.nombre FROM afw_activo af INNER JOIN afw_regional re ON af.regional_id = re.idregional WHERE descripcion LIKE %:descripcion%  ORDER BY af.fechacreacion DESC", nativeQuery=true)
 	public List<Map<String, Object>> buscaActivoDescripcion(@Param("descripcion") String descripcion);
 	
 	@Query(value="select TOP 1 idactivo from afw_activo where left(idactivo,2) = :idregional order by RIGHT(idactivo,7) desc", nativeQuery = true)
